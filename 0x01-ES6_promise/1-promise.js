035e96a7aa69176ac041d78f200fc70ee9000c7f
amasin76 <amasin76@gmail.com>
2024-01-03 23:01:45 +0000

    Fix(lint): no-unused-expressions

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,5 +1,6 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
+    // eslint-disable-next-line no-unused-expressions
     success
       ? resolve({ status: 200, body: 'Success' })
       : reject(new Error('The fake API is not working currently'));
