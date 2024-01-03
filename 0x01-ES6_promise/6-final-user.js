547c952664032c2dcf261d9480b0edf0f17b5d6e
amasin76 <amasin76@gmail.com>
2024-01-03 23:33:21 +0000

    Fix(lint): no-nested-ternary

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,17 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
+  /* eslint-disable no-nested-ternary */
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]);
+  ]).then((results) => results.map(({ status, value, reason }) => ({
+    status,
+    value:
+        status === 'rejected'
+          ? reason
+            ? `Error: ${reason.message}`
+            : 'Error'
+          : value,
+  })));
 }
